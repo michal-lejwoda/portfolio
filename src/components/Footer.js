b614f938296e666d03dbb61d62952190f554a6f1
michal <m.lejwoda@sunshineprofits.com>
2024-01-29 22:56:59 +0100

    Add Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,23 +14,20 @@ const Footer = () => {
     }
     return (
         <div className="footer__container">
-            <div className="footer__container__title"><h2>Kontakt</h2></div>
+            <div className="footer__container__title"><h2>Contact</h2></div>
             <div className="footer__container__boxes">
                 <div data-aos="fade-up" className="footer__container__boxes__mail">
                     <div className="footer__container__boxes__mail__title"><h2>Mail</h2></div>
                     <div className="footer__container__boxes__mail__icon"><FontAwesomeIcon icon={faEnvelope} size={"2x"}/></div>
-                    <div className="footer__container__boxes__mail__emailaddress">saxatachi@gmail.com</div>
-                
+                    <div className="footer__container__boxes__mail__emailaddress">michal.lejwoda@gmail.com</div>
                 </div>
                 <div data-aos="fade-up" onClick={() => click('https://github.com/saxatachi')} className="footer__container__boxes__github">
                     <div className="footer__container__boxes__github__title"><h2>Github</h2></div>
                     <div className="footer__container__boxes__github__icon"><img alt="github" src="./GitHub-Mark-32px.png"/></div>
                     <div className="footer__container__boxes__github__emailaddress">Kliknij Tutaj.</div>
-                
             </div>
-            
             </div>
-            <div className="footer__container__rest">Michał Lejwoda - Portfolio 2021r.</div>
+            <div className="footer__container__rest">Michał Lejwoda - Portfolio 2024r.</div>
         </div>
     )
 }
