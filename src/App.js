416e075d46b10306896f22b49a0a6ae5223c0388
michal <m.lejwoda@sunshineprofits.com>
2024-02-20 14:13:56 +0100

    move particles higher in components strructure

--- src/App.js
+++ src/App.js
@@ -1,67 +1,91 @@
-import React,{useRef} from "react"
+import React, {useEffect, useRef, useState} from "react"
 import './App.css';
 import '../src/sass/navbar.css'
-import Aboutme from  './components/Aboutme';
-import Footer from  './components/Footer';
-import Header from  './components/Header';
-import Projects from  './components/Projects';
-import Technologies from  './components/Technologies';
-import { faBars} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Aboutme from './components/Aboutme';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import Technologies from './components/Technologies';
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {Helmet} from "react-helmet";
+import {initParticlesEngine} from "@tsparticles/react";
+import {loadSlim} from "@tsparticles/slim";
 
 function App() {
-  const aboutmeRef = useRef(null);
-  const technologiesRef = useRef(null);
-  const projectsRef = useRef(null);
-  const contactRef = useRef(null)
-  const navLinks = useRef(null)
-  const executeScrollAboutMe = () => {
-    navLinks.current.classList.toggle("open")
-    aboutmeRef.current.scrollIntoView()
-  }
-  const executeScrollTechnologies = () => 
-  { 
-    navLinks.current.classList.toggle("open")
-    technologiesRef.current.scrollIntoView()
-  }
-  const executeScrollProjects = () => {
-    navLinks.current.classList.toggle("open")
-    projectsRef.current.scrollIntoView()
-  }
-  const executeScrollContact = () => {
-    navLinks.current.classList.toggle("open")
-    contactRef.current.scrollIntoView()
-  }
-  const hamburgerClick = () => {
-    navLinks.current.classList.toggle("open")
-    
-  }
-  return (
-    <div className="App">
-      <Helmet>
-        <title>Portfolio</title>
-        
-        <link rel="icon" type="mylogo.png" href="mylogo.ico" sizes="16x16" />
-      </Helmet>
-      <nav>
-        <div className="navbar">
-          <div className="navbar__menulogo" onClick={hamburgerClick}><FontAwesomeIcon icon={faBars} size="2x" /></div>   
-          <ul className="navbar__links" ref={navLinks}>
-            <li><div onClick={executeScrollAboutMe} >About me</div></li>
-            <li><div onClick={executeScrollTechnologies}>Technologies</div></li>
-            <li><div onClick={executeScrollProjects}>Projects</div></li>
-            <li><div onClick={executeScrollContact}>Contact</div></li>
-          </ul>
+    const aboutmeRef = useRef(null);
+    const technologiesRef = useRef(null);
+    const projectsRef = useRef(null);
+    const contactRef = useRef(null)
+    const navLinks = useRef(null)
+    const [init, setInit] = useState(false);
+
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadSlim(engine);
+        }).then(() => {
+            setInit(true);
+        });
+    }, []);
+
+    const executeScrollAboutMe = () => {
+        navLinks.current.classList.toggle("open")
+        aboutmeRef.current.scrollIntoView()
+    }
+    const executeScrollTechnologies = () => {
+        navLinks.current.classList.toggle("open")
+        technologiesRef.current.scrollIntoView()
+    }
+    const executeScrollProjects = () => {
+        navLinks.current.classList.toggle("open")
+        projectsRef.current.scrollIntoView()
+    }
+    const executeScrollContact = () => {
+        navLinks.current.classList.toggle("open")
+        contactRef.current.scrollIntoView()
+    }
+    const hamburgerClick = () => {
+        navLinks.current.classList.toggle("open")
+
+    }
+    return (
+        <div className="App">
+            <Helmet>
+                <title>Portfolio</title>
+
+                <link rel="icon" type="mylogo.png" href="mylogo.ico" sizes="16x16"/>
+            </Helmet>
+            <nav>
+                <div className="navbar">
+                    <div className="navbar__menulogo" onClick={hamburgerClick}><FontAwesomeIcon icon={faBars}
+                                                                                                size="2x"/></div>
+                    <ul className="navbar__links" ref={navLinks}>
+                        <li>
+                            <div onClick={executeScrollAboutMe}>About me</div>
+                        </li>
+                        <li>
+                            <div onClick={executeScrollTechnologies}>Technologies</div>
+                        </li>
+                        <li>
+                            <div onClick={executeScrollProjects}>Projects</div>
+                        </li>
+                        <li>
+                            <div onClick={executeScrollContact}>Contact</div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            {init && <div>
+            <section className="header"><Header executeScrollProjects={executeScrollProjects}/></section>
+            <section ref={aboutmeRef} className="aboutme"><Aboutme init={init}/></section>
+            <section ref={technologiesRef} className="technologies"><Technologies/></section>
+            <section ref={projectsRef} className="projects"><Projects/></section>
+            <section ref={contactRef} className="footer"><Footer/></section>
+                </div>
+    }
+
         </div>
-      </nav>
-      <section className="header"><Header executeScrollProjects={executeScrollProjects}/></section>
-      <section ref={aboutmeRef} className="aboutme"><Aboutme /></section>
-      <section ref={technologiesRef} className="technologies"><Technologies /></section>
-      <section ref={projectsRef} className="projects"><Projects /></section>
-      <section ref={contactRef} className="footer"><Footer /></section>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -2,30 +2,10 @@ import React, {useEffect, useState} from 'react'
 import '../sass/aboutme.css'
 import Aos from "aos";
 import "aos/dist/aos.css";
-import Particles, {initParticlesEngine} from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
+import Particles from "@tsparticles/react";
 
-const Aboutme = () => {
-    const [init, setInit] = useState(false);
+const Aboutme = (props) => {
 
-    // this should be run only once per application lifetime
-    useEffect(() => {
-        initParticlesEngine(async (engine) => {
-            // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-            // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-            // starting from v2 you can add only the features you need reducing the bundle size
-            //await loadAll(engine);
-            //await loadFull(engine);
-            await loadSlim(engine);
-            //await loadBasic(engine);
-        }).then(() => {
-            setInit(true);
-        });
-    }, []);
-
-    const particlesLoaded = (container) => {
-        console.log(container);
-    };
     useEffect(() => {
         Aos.init({duration: 2000})
 
@@ -40,9 +20,8 @@ const Aboutme = () => {
                 Python and Django framework. As a persistent and conscientious person in pursuit of my goals, I develop
                 my passion with daily, systematic work.
             </div>
-            {init && <Particles
+            {props.init && <Particles
                 id="tsparticles"
-                particlesLoaded={particlesLoaded}
                 options={{
                     background: {
                         color: {
