d526337a297715bd9fcad142221a21895f128486
michal <m.lejwoda@sunshineprofits.com>
2024-02-28 22:54:35 +0100

    add about me

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -13,12 +13,14 @@ const Aboutme = (props) => {
     return (
         <div data-aos="fade-right" className="aboutme__container">
             <div className="aboutme__container__title"><h2>About me</h2></div>
-            <div className="aboutme__container__description">My name is Michał Lejwoda. I graduated computer science
-                with a master's degree and currently i am trying to expand my programming skills. As a self-taught
-                programmer, I quickly grasp new technologies and frameworks. While i have experience with front end
-                frameworks like React, my true passion and preference lie in back end development especially using
-                Python and Django framework. As a persistent and conscientious person in pursuit of my goals, I develop
-                my passion with daily, systematic work.
+            <div className="aboutme__container__description">My name is Michał Lejwoda. I graduated with a master's
+                degree in computer science and i am currently trying to expand my programming skills. As a self-taught
+                programmer, I quickly grasp new technologies and frameworks, while i have experience with frontend
+                frameworks like React. My true passion and preference lie in backend development especially using Python
+                and Django framework. As a persistent and conscientious person in pursuit of my goals, I develop
+                my passion with daily, systematic work. In my free time, I like to do sports such as running or training
+                at the gym. I also like to play computer games doesn't matter if it is big-budget or an indie game.The
+                only kind of games that I can't convince myself to play are jrpg games :(
             </div>
             {props.init && <Particles
                 id="tsparticles"
